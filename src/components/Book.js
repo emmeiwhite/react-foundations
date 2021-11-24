71046aff466022147aa32c3af8f4931f82bbf16a
Imran Rather <emmeiwhite@gmail.com>
2021-11-24 11:48:06 +0530

    Synthetic Events - inline events

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,12 +2,34 @@ import React from "react";
 import "./../css/Book.css";
 
 const Book = ({ author, title, description, children }) => {
+  const handleMouseEnter = (e) => {
+    document.querySelectorAll(".bookWrapper").forEach((book) => {
+      e.target.style.color = "orange";
+    });
+  };
+
+  const handleMouseLeave = (e) => {
+    document.querySelectorAll(".bookWrapper").forEach((book) => {
+      e.target.style.color = "blue";
+    });
+  };
+
+  const handleButtonClick = (title) => {
+    alert(title);
+  };
+
   return (
-    <div className="bookWrapper">
+    <div
+      className="bookWrapper"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       <h3>{title}</h3>
       <h4>{author}</h4>
       <p>{description}</p>
       {children}
+
+      <button onClick={() => handleButtonClick(title)}>Click me</button>
     </div>
   );
 };
